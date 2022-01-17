30efb08ac98582ba39db6c3795e9229796c8e3dd
AlbinOdelstav <albin.odelstav@outlook.com>
2022-01-17 13:24:32 +0100

    Classes are now detected between classes in templates (iss4)

--- cssc.py
+++ cssc.py
@@ -187,19 +187,38 @@ def delete_all(css_location, css_list):
                     out_file.write(line)
 
 
+def get_result_keyword_arr(match):
+    match = match.replace("class=\"", "")
+    match = match.replace("id=\"", "")
+    match = match.replace("<", "")
+    match = match.replace(">", "")
+    match = match.replace("\"", "")
+    match = match.replace("\'", "")
+    return match.split()
+
+
 def scan_html_files(filenames, css_list):
     for i in range(len(css_list)):
         for filename in filenames:
             file = open(filename, mode='r', encoding='utf-8').read()
             if css_list[i]["type"] == "class":
-                reg = re.compile("class=." + re.escape(css_list[i]["value"]) + ".")
+                reg = re.compile("class=[\"|\'].+[\"|\']")
             elif css_list[i]["type"] == "id":
-                reg = re.compile("id=." + re.escape(css_list[i]["value"]) + ".")
+                reg = re.compile("id=[\"|\'].+[\"|\']")
             else:
                 reg = re.compile("<" + re.escape(css_list[i]["value"]))
-
-            if bool(re.search(reg, file)):
-                css_list[i]["used"] = True
+            results = re.findall(reg, file)
+            if results is None:
+                continue
+            for result in results:
+                result_arr = get_result_keyword_arr(result)
+                for keyword in result_arr:
+                    if css_list[i]["value"] == keyword:
+                        css_list[i]["used"] = True
+                        continue
+                if css_list[i]["used"]:
+                    continue
+            if css_list[i]["used"]:
                 continue
 
     css_list = list(filter(lambda x: x["used"] is False, css_list))
